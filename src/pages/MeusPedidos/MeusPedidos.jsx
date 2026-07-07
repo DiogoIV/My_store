ceebd86604019e363a3a3008dd280674b56577ab
DiogoIV <diogorodriguesdasilva156@gmail.com>
2026-07-07 17:56:36 -0300

    set
    Adicionando meus pedidos

--- src/pages/MeusPedidos/MeusPedidos.jsx
+++ src/pages/MeusPedidos/MeusPedidos.jsx
@@ -1,11 +1,46 @@
 import './meuspedidos.css'
-
+import { Link } from 'react-router-dom'
 
 
 function MeusPedidos() {
-    return(
+    return (
         <main className="principal-meuspedidos">
-            <h1>olá</h1>
+
+
+            <h1>Minhas Compras</h1>
+
+            <section className='container-lista-pedidos'>
+                <div className='lista-pedidos'>
+
+                    <div className='pedido-header'>
+
+                        <span>
+                            pedido: #1025
+                        </span>
+
+                        <span>
+                            Entregue
+                        </span>
+                    </div>
+
+                    <div className='pedido-body'>
+                        <span>06/07/2026</span>
+
+                        <span>Notebook</span>
+                    </div>
+
+                    <div className='pedido-footer'>
+                        <span>Total: R$ 4.350,500</span>
+
+                        <Link to="">
+                            Ver detalhes
+                        </Link>
+
+                    </div>
+                </div>
+            </section>
+
+
         </main>
     )
 }
