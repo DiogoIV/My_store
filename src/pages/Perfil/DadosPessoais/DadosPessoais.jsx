0cff518dc697054387dbaf96da78b51b01fdd7eb
DiogoIV <diogorodriguesdasilva156@gmail.com>
2026-09-19 21:21:12 -0300

    set
    terminando a página minha-conta, e começando dados pessoais.

--- src/pages/Perfil/DadosPessoais/DadosPessoais.jsx
+++ src/pages/Perfil/DadosPessoais/DadosPessoais.jsx
@@ -1,11 +1,47 @@
 
 import './dadosPessoais.css'
 
-function DadosPessoais () {
+function DadosPessoais() {
 
     return (
-        <main>
+        <main className='container-principal-dados'>
+
             <h1>Dados Pessoais</h1>
+
+            <section className='sessao-dados-valores'>
+
+                <h2>Informações pessoais</h2>
+
+
+                <ul>
+                    <li>
+                        <h3>Nome</h3>
+                        <span>Diogo</span>
+                    </li>
+
+                    <li>
+                        <h3>Sobrenome</h3>
+                        <span>Rodrigues</span>
+                    </li>
+
+                    <li>
+                        <h3>E-mail</h3>
+                        <span>diogo@email.com</span>
+                    </li>
+
+                    <li>
+                        <h3>Telefone</h3>
+                        <span>(11) 99999-9999</span>
+                    </li>
+
+                    <li>
+                        <button>
+                            Editar dados
+                        </button>
+                    </li>
+                </ul>
+
+            </section>
         </main>
     )
 }
