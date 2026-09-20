5484413e29b19d48743ce11be4660e12c8fbb146
DiogoIV <diogorodriguesdasilva156@gmail.com>
2026-09-20 17:17:18 -0300

    set
    termiando layout e conteúdo da primeira parte dos dados pessoais.

--- src/pages/Perfil/DadosPessoais/DadosPessoais.jsx
+++ src/pages/Perfil/DadosPessoais/DadosPessoais.jsx
@@ -13,35 +13,56 @@ function DadosPessoais() {
                 <h2>Informações pessoais</h2>
 
 
-                <ul>
-                    <li>
+                <ul className='dados-valores'>
+
+                    <li className='valores-pessoais'>
                         <h3>Nome</h3>
                         <span>Diogo</span>
                     </li>
 
-                    <li>
+                    <li className='valores-pessoais'>
                         <h3>Sobrenome</h3>
                         <span>Rodrigues</span>
                     </li>
 
-                    <li>
+                    <li className='valores-pessoais'>
                         <h3>E-mail</h3>
                         <span>diogo@email.com</span>
                     </li>
 
-                    <li>
+                    <li className='valores-pessoais'>
                         <h3>Telefone</h3>
                         <span>(11) 99999-9999</span>
                     </li>
 
-                    <li>
-                        <button>
+                    <li className='container-btn-editar-dados'>
+                        <button className='btn-editar-dados'>
                             Editar dados
                         </button>
                     </li>
                 </ul>
 
+                <div className='sessao-seguranca'>
+
+                    <h2>Segurança</h2>
+
+                    <div className='seguranca-valores'>
+
+                        <div className='valores-pessoais'>
+                            <h3>Senha</h3>
+                            <span>••••••••</span>
+                        </div>
+
+                        <button className='btn-alterar-senha'>
+                            Alterar senha
+                        </button>
+                    </div>
+
+                </div>
+
             </section>
+
+
         </main>
     )
 }
